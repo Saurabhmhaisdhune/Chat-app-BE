924467a8e0a603151d0eb8d75a144d4f78ba5e19
saurabh <saurabhmhaisdhune1@gmail.com>
2022-11-14 16:16:49 -1200

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const messages = require("./routes/messages");
+const app= express();
+const socket = require("socket.io");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth",userRoutes)
+app.use("/api/messages",messages)
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log("DB Connected Successfully");
+}).catch((err)=>{
+    console.log(err.message);
+});
+
+const server = app.listen(process.env.PORT,()=>{
+    console.log(`Server is connected to Port ${process.env.PORT}`)
+});
+
+const io = socket(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      credentials: true,
+    },
+  });
+  
+  global.onlineUsers = new Map();
+  io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+      onlineUsers.set(userId, socket.id);
+    });
+  
+    socket.on("send-msg", (data) => {
+      const sendUserSocket = onlineUsers.get(data.to);
+      if (sendUserSocket) {
+        socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+      }
+    });
+  });
---
+++ routes/userRoutes.js
@@ -0,0 +1,7 @@
+const { register, login, setAvatar, getAllUsers } = require("../controllers/userController");
+const router = require("express").Router();
+router.post("/register", register);
+router.post("/login", login);
+router.post("/setavatar/:id", setAvatar);
+router.get("/allusers/:id", getAllUsers);
+module.exports = router;
