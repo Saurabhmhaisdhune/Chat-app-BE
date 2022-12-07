e3a2ccd5cf2cf9acdde3ecdf03e1957547054cba
saurabh <saurabhmhaisdhune1@gmail.com>
2022-12-06 21:11:32 -1200

    upo

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const server = app.listen(process.env.PORT,()=>{
 
 const io = socket(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "0",
       credentials: true,
     },
   });
