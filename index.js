5ac7a2d5f8c04ecad888747a98fd1783a5ece80c
saurabh <saurabhmhaisdhune1@gmail.com>
2022-12-06 21:32:29 -1200

    update

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const server = app.listen(process.env.PORT,()=>{
 
 const io = socket(server, {
     cors: {
-      origin: "*",
+      origin: "http://localhost:7010",
       credentials: true,
     },
   });
