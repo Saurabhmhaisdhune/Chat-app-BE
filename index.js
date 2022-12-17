65a70134322e87881fa0c1f11fc170c890da06c8
saurabh <saurabhmhaisdhune1@gmail.com>
2022-12-17 10:58:08 -1200

    cors updates

--- index.js
+++ index.js
@@ -28,8 +28,7 @@ const server = app.listen(process.env.PORT,()=>{
 
 const io = socket(server, {
     cors: {
-      origin: "http://localhost:7010",
-      credentials: true,
+      origin: '*'
     },
   });
   
